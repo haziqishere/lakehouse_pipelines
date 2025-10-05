8d386f2e449cddfab669b21025cee3bff3f8bd8a
Haziq Hakimi Mazlisham <95128744+haziqishere@users.noreply.github.com>
2025-10-05 09:41:56 +0800

    Run job successfully but got internal iceberg issue

--- dags/dbt_trino/models/curated/fact_sale.sql
+++ dags/dbt_trino/models/curated/fact_sale.sql
@@ -10,7 +10,7 @@ SELECT
     (s.order_quantity * p.product_price) - (s.order_quantity * p.product_cost) as profit
 
 FROM {{ ref('stg_sales')}} s
-LEFT JOIN {{ ref('dim_products')}} p
+LEFT JOIN {{ ref('dim_product')}} p
     ON s.productkey = p.productkey
 LEFT JOIN {{ ref('stg_territories')}} t
     ON s.territorykey = t.territorykey
